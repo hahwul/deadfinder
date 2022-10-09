e9456fcbbd5d514902d67e05d81bd1025bdcbc2f
hahwul <hahwul@gmail.com>
2022-10-09 18:06:40 +0900

    Add github action

---
+++ github-action/README.md
@@ -0,0 +1,1 @@
+## DeadFinder Github Action
---
+++ github-action/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+# $1 : commands
+# $2 : target
+# -------------
+
+export df=/usr/local/bundle/gems/deadfinder-*/bin/deadfinder
+
+case $1 in
+	'file')
+		$df file $2 -o output.json
+		;;
+	'url')
+		$df url $2 -o output.json
+		;;
+	'sitemap')
+		$df sitemap $2 -o output.json
+		;;
+esac
+
+out=$(cat /output.json)
+echo "::set-output name=output::$out"
