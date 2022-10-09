706c824ccfca94a8e31a46f2d0a6f47976d92a5c
HAHWUL <hahwul@gmail.com>
2022-10-10 00:03:04 +0900

    Update entrypoint.sh

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -8,13 +8,13 @@ export df=/usr/local/bundle/gems/deadfinder-*/bin/deadfinder
 
 case $1 in
 	'file')
-		$df file $2 -o output.json
+		$df file $2 -o /output.json
 		;;
 	'url')
-		$df url $2 -o output.json
+		$df url $2 -o /output.json
 		;;
 	'sitemap')
-		$df sitemap $2 -o output.json
+		$df sitemap $2 -o /output.json
 		;;
 esac
 
