47cd5831a63eb575780c2ffc305106caddb6165f
port19 <port19@port19.xyz>
2023-08-09 09:23:58 +0200

    fix false positive on mailto links

--- lib/deadfinder/utils.rb
+++ lib/deadfinder/utils.rb
@@ -11,6 +11,8 @@ def generate_url(text, base_url)
         return "#{uri.scheme}:#{node}"
       elsif node.start_with? '/'
         return "#{uri.scheme}://#{uri.host}#{node}"
+      elsif node.start_with? 'mailto:'
+        return nil
       else
         return "#{uri}#{node}"
       end
