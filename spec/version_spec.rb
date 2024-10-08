f178f1b358ae3be784d0286a9aeb0fe1df387d35
HAHWUL <hahwul@gmail.com>
2024-10-09 00:07:41 +0900

    Add version_spec.rb for testing the VERSION constant
    Signed-off-by: HAHWUL <hahwul@gmail.com>

---
+++ spec/version_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'uri'
+require_relative '../lib/deadfinder/version'
+
+RSpec.describe 'Version' do
+  describe 'VERSION' do
+    it 'returns the correct version' do
+      expect(VERSION).nil?
+    end
+  end
+end
