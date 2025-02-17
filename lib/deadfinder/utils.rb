951cec95fce6063e2d61ff29b0df8a492ff3b3fb
HAHWUL <hahwul@gmail.com>
2025-02-17 11:31:03 +0900

    ♻️ Code Refactoring

--- lib/deadfinder/utils.rb
+++ lib/deadfinder/utils.rb
@@ -4,41 +4,24 @@ require 'uri'
 
 def generate_url(text, base_url)
   node = text.to_s
+  return node if node.start_with?('http://', 'https://')
+
   begin
-    unless node.start_with?('http://', 'https://')
-      uri = URI(base_url)
-      if node.start_with? '//'
-        return "#{uri.scheme}:#{node}"
-      elsif node.start_with? '/'
-        return "#{uri.scheme}://#{uri.host}#{node}"
-      elsif ignore_scheme? node
-        return nil
-      else
-        return "#{extract_directory(uri)}#{node}"
-      end
+    uri = URI(base_url)
+    if node.start_with?('//')
+      "#{uri.scheme}:#{node}"
+    elsif node.start_with?('/')
+      "#{uri.scheme}://#{uri.host}#{node}"
+    elsif ignore_scheme?(node)
+      nil
+    else
+      URI.join(base_url, node).to_s
     end
   rescue StandardError
-    # puts e
+    nil
   end
-  node
 end
 
 def ignore_scheme?(url)
   url.start_with?('mailto:', 'tel:', 'sms:', 'data:', 'file:')
-end
-
-def extract_directory(uri)
-  return "#{uri.scheme}://#{uri.host}#{uri.path}" if uri.path.end_with?('/')
-
-  path_components = uri.path.split('/')
-  path_components.last
-  path_components.pop
-
-  directory_path = path_components.join('/')
-
-  if directory_path.start_with?('/')
-    "#{uri.scheme}://#{uri.host}#{directory_path}/"
-  else
-    "#{uri.scheme}://#{uri.host}/#{directory_path}/"
-  end
 end
