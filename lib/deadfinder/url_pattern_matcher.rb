90db292c588d6b0e520c22e7ad3e94c193cda916
HAHWUL <hahwul@gmail.com>
2025-03-09 22:31:07 +0900

    Defending ReDOS attacks

--- lib/deadfinder/url_pattern_matcher.rb
+++ lib/deadfinder/url_pattern_matcher.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
+require 'timeout'
+
 module DeadFinder
   # URL pattern matcher module
   module UrlPatternMatcher
     def self.match?(url, pattern)
-      Regexp.new(pattern).match?(url)
+      Timeout.timeout(1) { Regexp.new(pattern).match?(url) }
+    rescue Timeout::Error
+      false
     end
 
     def self.ignore?(url, pattern)
-      Regexp.new(pattern).match?(url)
+      Timeout.timeout(1) { Regexp.new(pattern).match?(url) }
+    rescue Timeout::Error
+      false
     end
   end
 end
